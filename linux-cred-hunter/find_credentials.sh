5a63f8a092c972cf238d0a03b61e9eafbc5e37a1
fairyknight-FR <fairyknight210@gmail.com>
2025-07-24 00:51:22 +0530

    Create find_credentials.sh

---
+++ linux-cred-hunter/find_credentials.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Color definitions
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}[*] Searching for common credential files on the system...${NC}"
+echo
+
+# List of potential credential file paths (some use wildcards)
+files=(
+  "/etc/passwd"
+  "/etc/shadow"
+  "/etc/group"
+  "/etc/gshadow"
+  "/var/log/auth.log"
+  "/etc/mysql/my.cnf"
+  "/var/www/html/*config*.php"
+  "/home/*/.bash_history"
+  "/home/*/.ssh/id_rsa"
+  "/home/*/.ssh/known_hosts"
+  "/root/.bash_history"
+  "/home/*/.netrc"
+  "/home/*/.git-credentials"
+  "/home/*/.aws/credentials"
+  "/home/*/.config/**"
+  "/etc/openvpn/*.conf"
+  "/etc/samba/smb.conf"
+  "/var/www/html/wp-config.php"
+  "/opt/**"
+)
+
+# Search and display results
+for path in "${files[@]}"; do
+  matches=$(find $path 2>/dev/null)
+  if [[ ! -z "$matches" ]]; then
+    echo -e "${GREEN}[+] Found:${NC} $path"
+    echo "$matches" | sed 's/^/    - /'
+    echo
+  else
+    echo -e "${RED}[-] Not Found:${NC} $path"
+  fi
+done
+
+echo -e "${GREEN}[*] Done.${NC}"
